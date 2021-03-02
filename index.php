0663f87af888525af9996333b25060aee2be87ed
itsfakku <69498553+itsfakku@users.noreply.github.com>
2021-03-02 14:04:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css"/>
+
+	<link href="https://scontent.fhan2-6.fna.fbcdn.net/v/t1.0-9/52382647_2013649935603549_6570728925503160320_n.jpg?_nc_cat=104&_nc_sid=7aed08&_nc_ohc=oJjnga_RXr0AX_7orKA&_nc_ht=scontent.fhan2-6.fna&oh=1f3c65efbb7c453fd85c05b366336fc2&oe=5F93902B" rel="icon">
+
+    <title>pornhub.com</title>
+    <style>
+    	body{
+    		background-color: #f7f7f7;
+    		font-family: Open sans;
+    	}
+    	img{
+    		width: 100%;
+    		min-height: 250px;
+    	}
+    	.search-container{
+    		margin-left: 300px;
+    	}
+    	.main{
+    		padding: 40px 0;
+    	}
+    	.col-md-3{
+    		margin-bottom: 40px;
+    	}
+    	.album-poster{
+    		position: relative;
+    		display: block;
+    		border-radius: 7px;
+    		overflow: hidden;
+    		box-shadow: 0 15px 35px #3d2173a1;
+    		transition: all ease 0.4s;
+    	}
+    	.album-poster:hover{
+    		box-shadow: none;
+    		transform: scale(0.98) translateY(5px);
+    	}
+    	h3{
+    		font-size: 34px;
+    		margin-bottom: 34px;
+    		border-bottom: 4px solid #e6e6e6;
+    		padding-bottom: 15px;
+    	}
+    	p{
+    		font-size: 15px;
+    	}
+    	h4{
+    		font-size: 16px;
+    		text-transform: uppercase;
+    		margin-top: 15px;
+    		font-weight: 700;
+    	}
+		
+
+		#aplayer{
+			position: fixed;
+			bottom: -100%;
+			left: 0;
+			width: 100%;
+			margin: 0;
+			box-shadow: 0 -2px 2px #dadada;
+			background-color: #fff;
+			transition: all ease 0.5s;
+		}
+		#aplayer.showPlayer{
+			bottom: 0;
+		}
+
+		span{
+			color: #000 !important;
+			font-size: 16px;
+		}
+		.aplayer .aplayer-info .aplayer-controller .aplayer-bar-wrap .aplayer-bar .aplayer-loaded{
+			background: #e0e0e0;
+    		height: 4px;
+		}
+		.aplayer .aplayer-info .aplayer-controller .aplayer-bar-wrap .aplayer-bar .aplayer-played{
+			height: 4px;
+			background-color: #2196F3 !important;
+		}
+		.aplayer .aplayer-info .aplayer-controller .aplayer-bar-wrap .aplayer-bar .aplayer-played .aplayer-thumb{
+			background-color: #2196F3 !important;
+		}
+
+		.aplayer .aplayer-icon{
+			width: 20px;
+			height: 20px;
+		}
+		.aplayer .aplayer-info .aplayer-controller .aplayer-time .aplayer-icon path {
+		    fill: #000;
+		}
+		.aplayer .aplayer-info .aplayer-music{
+			margin-bottom: 5px;
+		}
+
+
+    </style>
+ 	</head>
+	<body>
+		
+<nav class="navbar">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#"> <span class="glyphicon glyphicon-user"></span>Trang chủ</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>Sản phẩm</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>Liên hệ</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li style="font-size:16px" class="dropdown">
+                        <a data-toggle="dropdown" href="">Tài khoản<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="login.php">Đăng nhập</a></li>
+                            <li><a href="register.php">Đăng ký</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+</nav>
+
+<div class="search-container">
+	<form action="/action_page.php">
+		<input type="text" placeholder="search.." name="search">
+		<button type="submit"><i class="fa fa-search"></i></button>
+	</form>	
+</div>
+
+<div class="main">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h3>Riot Entertainment</h3>
+			</div>
+			<div class="col-md-3">
+				<a href="javascript:void();" class="album-poster" data-switch="0">
+					<img src="https://avatar-nct.nixcdn.com/song/2018/11/04/2/0/7/7/1541268104862_640.jpg" alt="">
+				</a>
+				<h4>Pop/Stars</h4>
+				<p>K/DA - (ft. Madison Beer, (G)I-DLE, Jaira Burns) (Album 2018)</p>
+			</div>
+			<div class="col-md-3">
+				<a href="javascript:void(1);" class="album-poster" data-switch="1">
+					<img src="https://i1.sndcdn.com/artworks-000476343180-a8uhcr-t500x500.jpg" alt="">
+				</a>
+				<h4>Awaken</h4>
+				<p>Valerie Broussard</p>
+			</div>
+			<div class="col-md-3">
+				<a href="#" class="album-poster" data-switch="2">
+					<img src="https://avatar-nct.nixcdn.com/playlist/2017/11/02/c/d/5/b/1509587114293_500.jpg" alt="">
+				</a>
+				<h4>Legends Never Die</h4>
+				<p>Against The Current</p>
+			</div>
+
+			<div class="col-md-3">
+				<a href="#" class="album-poster" data-switch="3">
+					<img src="https://avatar-nct.nixcdn.com/song/2019/11/15/0/5/0/1/1573800658233_640.jpg" alt="">
+				</a>
+				<h4>GIANTS</h4>
+				<p>True Damage - (ft. Becky G, Keke Palmer, SOYEON, DUCKWRTH, Thutmose)</p>
+			</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h3>......</h3>
+			</div>
+			<div class="col-md-3">
+				<a href="#" class="album-poster" data-switch="4">
+					<img src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>1</h4>
+				<p>1</p>
+			</div>
+			<div class="col-md-3">
+				<a href="#" class="album-poster" data-switch="5">
+					<img src="https://images.pexels.com/photos/210922/pexels-photo-210922.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>2</h4>
+				<p>2</p>
+			</div>
+			<div class="col-md-3">
+				<a href="#" class="album-poster" data-switch="6">
+					<img src="https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>3</h4>
+				<p>3</p>
+			</div>
+			<div class="col-md-3">
+				<a href="#" class="album-poster">
+					<img src="https://images.pexels.com/photos/1370545/pexels-photo-1370545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>4</h4>
+				<p>4</p>
+			</div>
+		</div>
+		</div>
+
+
+		<div class="row">
+			<div class="col-md-12">
+				<h3>coming soon</h3>
+			</div>
+			<div class="col-md-2">
+				<a href="#" class="album-poster">
+					<img src="https://images.pexels.com/photos/1699161/pexels-photo-1699161.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>5</h4>
+			</div>
+			<div class="col-md-2">
+				<a href="#" class="album-poster">
+					<img src="https://images.pexels.com/photos/838702/pexels-photo-838702.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>6</h4>
+			</div>
+			<div class="col-md-2">
+				<a href="#" class="album-poster">
+					<img src="https://images.pexels.com/photos/894156/pexels-photo-894156.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>7</h4>
+			</div>
+			
+			<div class="col-md-2">
+				<a href="#" class="album-poster">
+					<img src="https://images.pexels.com/photos/2118046/pexels-photo-2118046.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>8</h4>
+			</div>
+			<div class="col-md-2">
+				<a href="#" class="album-poster">
+					<img src="https://images.pexels.com/photos/1735240/pexels-photo-1735240.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>9</h4>
+			</div>
+			<div class="col-md-2">
+				<a href="#" class="album-poster">
+					<img src="https://images.pexels.com/photos/2272854/pexels-photo-2272854.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+				</a>
+				<h4>10</h4>
+			</div>
+		</div>
+
+
+	</div>
+</div>
+
+	<div id="aplayer"></div>
+
+	<!-- Jquery Link -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+	<!-- Bootstrap Link -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<!-- APlayer Jquery link -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js"></script>	
+
+	<script>
+
+
+		// NOW I CLICK album-poster TO GET CURRENT SONG ID
+		$(".album-poster").on('click', function(e){
+			var dataSwitchId = $(this).attr('data-switch');
+			//console.log(dataSwitchId);
+
+			// and now i use aplayer switch function see
+			ap.list.switch(dataSwitchId); //this is static id but i use dynamic 
+
+			// aplayer play function
+			// when i click any song to play
+			ap.play();
+
+			// click to slideUp player see
+			$("#aplayer").addClass('showPlayer');
+		});
+
+		const ap = new APlayer({
+		    container: document.getElementById('aplayer'),
+		    listFolded: true,
+		    audio: [
+		    {
+		        name: 'Pop/Stars',// SONG NAME
+		        artist: 'K/DA',//ARTIST NAME
+		        url: 'songFile/KDA - POPSTARS (ft. Madison Beer, (G)I-DLE, Jaira Burns).mp3',// PATH NAME AND SONG URL
+		        cover: 'https://avatar-nct.nixcdn.com/song/2018/11/04/2/0/7/7/1541268104862_640.jpg' // COVER images
+		    },
+			{
+		        name: 'Awaken',  
+		        artist: 'Valerie Broussard', 
+		        url: 'songFile/Awaken (ft. Valerie Broussard).mp3', 
+		        cover: 'https://i1.sndcdn.com/artworks-000476343180-a8uhcr-t500x500.jpg'
+		    },
+			{	
+				name: 'Legends Never Die',
+				artist: 'Against The Current',
+				url: 'songFile/Legends Never Die (ft. Against The Current).mp3',
+				cover: 'https://avatar-nct.nixcdn.com/playlist/2017/11/02/c/d/5/b/1509587114293_500.jpg',
+			},
+			{
+				name: 'GIANTS',
+				artist: 'Becky G, Keke Palmer, SOYEON, DUCKWRTH, Thutmose',
+				url: 'songFile/True Damage - GIANTS (ft. Becky G, Keke Palmer, SOYEON, DUCKWRTH, Thutmose).mp3',
+				cover: 'https://avatar-nct.nixcdn.com/song/2019/11/15/0/5/0/1/1573800658233_640.jpg',
+			},
+
+
+		    ]
+		});
+	</script>
+<article>
+  <div class="menu">
+				<ul>                   
+                   
+					<li><a href="index_product.php?quanly=search_product">Thêm, sửa, xóa, tìm kiếm và nghe nhạc</a></li>
+                    <li><a href="http://localhost/gaucon/simple-shopping-cart/">Thêm nhạc vào giỏi hàng</a></li>
+				</ul>	
+
+			 <div class="content_product"><?php require("content_product.php"); ?></div>
+		</div>	
+</article>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,63 @@
+<style>
+  
+</style>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<div class="register_box">
+  <form method = "post" action="" enctype="multipart/form-data">
+    <table align="left" width="70%">
+      <tr align="left">
+        <td colspan="4"><h2>Register.</h2>
+          <br />
+          <span> Already have account? <a href="login.php">Login Now.</a><br />
+          <br />
+          </span></td>
+      </tr>
+      <tr>
+        <td width="15%"><b>UserName:</b></td>
+        <td colspan="3"><input type="text" name="username" required placeholder="UserName" /></td>
+      </tr>
+      <tr>
+        <td width="15%"><b>Password:</b></td>
+        <td colspan="3"><input type="password" id="password_confirm1" name="password" required placeholder="Password" /></td>
+      </tr>
+      <tr>
+        <td width="15%"><b>Confirm Password:</b></td>
+        <td colspan="3"><input type="password" id="password_confirm2" name="confirm_password" required placeholder="Confirm Password" />
+      </tr>
+      <tr align="left">
+        <td></td>
+        <td colspan="4"><input type="submit" name="register" value="Register" /></td>
+      </tr>
+    </table>
+  </form>
+</div>
+<?php
+$con = new mysqli('localhost', 'root', '', 'music');
+if (!$con) {
+    echo "ket noi that bai";
+}
+if (isset($_POST['register'])) {
+    if (!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+        $check_exist = mysqli_query($con, "select * from users where username = '$username'");
+        $username_count = mysqli_num_rows($check_exist);
+        $row_register = mysqli_fetch_array($check_exist);
+        if ($username_count > 0) {
+            echo "<script>alert('Sorry, your username already exist in our database !')</script>";
+        } elseif ($row_register['username'] != $username && $password == $confirm_password) {
+            $run_insert = mysqli_query($con, "insert into users values ('$username','$password') ");
+            if ($run_insert) {
+                echo "<script>alert('Account has been created successfully!')</script>";
+                echo "<script>window.open('login.php','_self')</script>";
+            }
+        }
+    }
+}
+?>
+
+</div>
